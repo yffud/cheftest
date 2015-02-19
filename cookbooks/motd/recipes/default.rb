b590c6196c4ca348e9ca1908394879e577cd70e5
root <root@ip-10-185-228-67.ec2.internal>
2015-02-19 20:16:30 +0000

    Change to use templateswq

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+file "/etc/motd" do
+ action :create
+ #content "Property of COMPANY NAME"
+ source "motd.erb"
+ mode "0644"
+ owner "root"
+ group "root"
+end
